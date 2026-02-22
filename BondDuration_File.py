369b7cb55f957d121b0ad5e906b3d9671f51b16d
Jarven-Xu <jx2673@columbia.edu>
2026-02-22 18:29:19 -0500

    Update BondDuration_File.py

--- BondDuration_File.py
+++ BondDuration_File.py
@@ -1,27 +1,16 @@
-
 def getBondDuration(y, face, couponRate, m, ppy = 1):
-
     r = y
     c = face * couponRate
     n = m
-
     bondPrice = 0
     weightedPV = 0
-
     for t in range(1, n + 1):
-
         pv = c / (1 + r) ** t
-
         bondPrice += pv
         weightedPV += t * pv
-
-
     pvFace = face / (1 + r) ** n
-
     bondPrice += pvFace
     weightedPV += n * pvFace
-
-
     bondDuration = weightedPV / bondPrice
-
     return bondDuration
+
