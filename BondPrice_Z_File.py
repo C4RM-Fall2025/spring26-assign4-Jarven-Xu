a9e608649b4bd6f084d3010ab7a8dd913fd67411
Jarven-Xu <jx2673@columbia.edu>
2026-02-22 18:30:30 -0500

    Update BondPrice_Z_File.py

--- BondPrice_Z_File.py
+++ BondPrice_Z_File.py
@@ -1,20 +1,11 @@
-
-
 def getBondPrice_Z(face, couponRate, times, yc):
-
     c = face * couponRate
     bondPrice = 0
-
     n = len(times)
-
     for i, (t, y) in enumerate(zip(times, yc)):
-
         if i < n - 1:
             cashflow = c
         else:
             cashflow = c + face
-
         bondPrice += cashflow / (1 + y) ** t
-
-
     return bondPrice
