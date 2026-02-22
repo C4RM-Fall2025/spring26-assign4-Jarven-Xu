7a44562b0de9ab8c9405caea3aac597259756ffc
Jarven-Xu <jx2673@columbia.edu>
2026-02-22 18:30:50 -0500

    Update FizzBuzz_File.py

--- FizzBuzz_File.py
+++ FizzBuzz_File.py
@@ -1,22 +1,14 @@
-
-
 def FizzBuzz(start, finish):
-
     outlist = []
-
     for i in range(start, finish + 1):
-
         if i % 15 == 0:
             outlist.append("FizzBuzz")
-
         elif i % 3 == 0:
             outlist.append("Fizz")
-
         elif i % 5 == 0:
             outlist.append("Buzz")
-
         else:
             outlist.append(i)
-
     return outlist
+
 
